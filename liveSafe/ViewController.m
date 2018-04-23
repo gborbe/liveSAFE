e1bf4f0073818947a30773cfadb4c74bb85b3f01
Garrett Borbe <gborbe@D-10-156-21-167.dhcp4.washington.edu>
2018-04-23 12:06:52 -0700

    Update with map pins and database functions

--- liveSafe/ViewController.m
+++ liveSafe/ViewController.m
@@ -56,10 +56,17 @@
             
             //Bring this dictionary over to create new object for Organization class
             Organizations *org = [[Organizations alloc] init];
+            
             org.name = [NSString stringWithString:[entry objectForKey:@"name"]];
             org.address = [NSString stringWithString:[entry objectForKey:@"address"]];
             org.phone = [NSString stringWithString:[entry objectForKey:@"phone"]];
             org.website = [NSString stringWithString:[entry objectForKey:@"website"]];
+            org.space = [NSString stringWithString:[entry objectForKey:@"space"]];
+            org.openHour = [NSString stringWithString:[entry objectForKey:@"openTime"]];
+            org.closeHour = [NSString stringWithString:[entry objectForKey:@"closeTime"]];
+            org.servicesDescription = [NSString stringWithString:[entry objectForKey:@"servicesDescription"]];
+            org.requirements = [NSString stringWithString:[entry objectForKey:@"requirements"]];
+            org.imgURL = [NSString stringWithString:[entry objectForKey:@"img"]];
             
             [self.orgObjectLibrary addObject:org];
 
@@ -136,6 +143,7 @@ didSignInWithUser:(nullable FIRUser *)user
     }
 }
 
+<<<<<<< HEAD
 - (void)locationOpen {
     //Get hours from Database in this format
     NSString *open = @"08:00 AM";
@@ -158,4 +166,51 @@ didSignInWithUser:(nullable FIRUser *)user
     NSLog(@"Close: %@",[formatTime stringFromDate:closeDate]);
 }
 
+=======
+//- (void)locationOpen {
+//    //Get hours from Database in this format
+//    NSString *open = @"08:00 AM";
+//    NSString *close = @"10:00 PM";
+//
+//    //Get current time
+//    NSDate *nowDate = [[NSDate alloc] init];
+//
+//    //Date Formatter
+//    NSDateFormatter *formatTime = [[NSDateFormatter alloc]init];
+//    [formatTime setDateFormat:@"hh:mm a"];
+//
+//    //Open & Close NSString hours to NSDate
+//    NSDate *openDate = [formatTime dateFromString:open];
+//    NSDate *closeDate = [formatTime dateFromString:close];
+//
+//    //Testing
+//    NSLog(@"Current Time: %@",[formatTime stringFromDate:nowDate]);
+//    NSLog(@"Open: %@",[formatTime stringFromDate:openDate]);
+//    NSLog(@"Close: %@",[formatTime stringFromDate:closeDate]);
+//
+//    //Check to see if location is open
+//    int openMin = [self minutesSinceMidnight:openDate];
+//    int closeMin = [self minutesSinceMidnight:closeDate];
+//    int nowMin = [self minutesSinceMidnight:nowDate];
+//
+//    if (nowMin < openMin && nowMin > closeMin) {
+//        NSLog(@"This location is open");
+//    } else if (nowMin > openMin && nowMin < closeMin) {
+//        NSLog(@"This location is open");
+//    } else {
+//        NSLog(@"This location is closed");
+//    }
+//
+//}
+//
+//-(int) minutesSinceMidnight:(NSDate *)date
+//{
+//    NSCalendar *gregorian = [[NSCalendar alloc]
+//                             initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+//    unsigned unitFlags =  NSCalendarUnitHour | NSCalendarUnitMinute;
+//    NSDateComponents *components = [gregorian components:unitFlags fromDate:date];
+//
+//    return 60 * [components hour] + [components minute];
+//}
+>>>>>>> Added time methods and updated UITableView
 @end
